00128784c0c43377f521346d15ba48ebb3039fbe
Juliagali <juliagalibarroso@gmail.com>
2022-10-27 15:34:41 -0300

    react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="imagem Tela inicial" className="img"></img>
+        </>
+    );
+}
+
+export default Home;
