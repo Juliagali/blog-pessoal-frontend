5b02f70461ccf45d37f93f4a85d19aff58cb8ada
Juliagali <juliagalibarroso@gmail.com>
2022-11-01 17:58:06 -0300

    react router e componente login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import Navbar from'./components/estaticos/navbar/Navbar';
+import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import { Grid } from "@material-ui/core"
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <>
-    <Navbar />
-      <Home />
-      <Footer />
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Login />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/home" element={<Home />}/>
+          </Routes>
+        <Footer />
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
